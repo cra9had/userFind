2d40485a626e2aeb206885e98bc160be16b6b278
Khaiam <khaiam@mail.ru>
2023-12-10 13:39:59 +0300

    environ

--- web/settings.py
+++ web/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = 'django-insecure-uaari#dgx@t0x#g($ndn#+5h9_r^lc%n%=nk8l#ol_e9h()sqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
