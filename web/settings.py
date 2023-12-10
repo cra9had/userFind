baa82fa9a0579df4364377b852e42441a32c4f27
Khaiam <khaiam@mail.ru>
2023-12-10 16:49:57 +0300

    static root

--- web/settings.py
+++ web/settings.py
@@ -152,10 +152,12 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
