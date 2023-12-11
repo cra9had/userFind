62b829411e0afa7da8b0f4fbe12aa98d01ca41fb
Khaiam <khaiam@mail.ru>
2023-12-11 23:42:57 +0300

    js fixed

--- static/js/common.js
+++ static/js/common.js
@@ -1,9 +1,9 @@
-var API_URL = 'http://191.96.53.250/api/';
+var api_url = 'http://191.96.53.250/api/';
 
 // Check if the environment is local (you can adjust this condition based on your setup)
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     // Override API URL for local development
-    API_URL = 'http://localhost:8000/api/';  // Change this to your local server URL
+    api_url = 'http://localhost:8000/api/';  // Change this to your local server URL
 }
 
 
